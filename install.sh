b6c3391529858989e74bbcd3dec12361f433b13f
Matt Cordial <matt.cordial@gmail.com>
2019-12-29 14:23:40 -0500

    add neovim init

--- install.sh
+++ install.sh
@@ -5,24 +5,21 @@ cd ~
 
 function symlinkifne {
 
-    if [[ -e $1 ]]; then
-        # file exists
+  if [[ -e $1 ]]; then
+    # file exists
 
-        if [[ -L $1 ]]; then
-            # it's already a simlink, assume from this proj
-            echo "simlink $1 exists, skipped"
-            return
-        fi
-
-        if [[ ! -e ~/.dotfiles_backup/$1 ]];then
-	    # backup file does not exist yet
-            mv $1 ~/.dotfiles_backup/
-            echo "backed up existing $1 ..."
-        fi
+    if [[ -L $1 ]]; then
+      # it's already a simlink, assume from this proj
+      echo "simlink $1 exists, skipped"
+      return
+    else
+      mv $1 ~/.dotfiles_backup/
+      echo "backed up existing $1 ..."
     fi
-    # create the link
-    ln -s ~/.dotfiles/$1 $1
-    echo "linked $1"
+  fi
+  # create the link
+  ln -s ~/.dotfiles/$1 $1
+  echo "linked $1"
 }
 
 symlinkifne .gitconfig
@@ -31,3 +28,6 @@ symlinkifne .vim
 symlinkifne .vimrc
 symlinkifne .zshrc
 symlinkifne .editorconfig
+
+mkdir -p ~/.config/nvim
+symlinkifne .config/nvim/init.vim
