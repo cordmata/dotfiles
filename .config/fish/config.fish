5f2c0903aee57f58ed65666a1b269328ad31a7ad
Matt Cordial <Matt.Cordial@experian.com>
2021-08-27 13:11:13 -0700

    add aliases and change name of variable

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,6 +16,10 @@ alias ls="ls -G"
 abbr -a f open -a Finder ./
 abbr -a gw ./gradlew
 abbr -a tf terraform
+abbr -a tfp terraform plan
+abbr -a tfpo terraform plan -out plan
+abbr -a tfsl terraform state list
+abbr -a tfsh terraform show
 abbr -a tg terragrunt
 abbr -a dstop 'docker stop (docker ps -aq)'
 abbr -a kc kubectl
@@ -25,6 +29,7 @@ abbr -a gfa git fetch --all --prune --jobs=10
 abbr -a gco git checkout
 abbr -a gcd git checkout develop
 abbr -a gcm git checkout master
+abbr -a gst git status
 abbr -a gsw git switch
 abbr -a gswc git switch -c
 
