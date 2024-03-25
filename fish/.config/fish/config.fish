29ba5a3ab0f3d3cae3ff42978fccdf309e14c6d7
Matt Cordial <matt.cordial@experian.com>
2024-03-25 13:00:46 -0700

    Add sbin homebrew path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,7 +4,7 @@ set -Ux VAULT_SKIP_VERIFY 1
 set -Ux LENS_DISABLE_GPU true
 set -Ux XDG_CONFIG_HOME "$HOME/.config"
 
-fish_add_path '/Applications/Visual Studio Code.app/Contents/Resources/app/bin' '/Applications/IntelliJ IDEA.app/Contents/MacOS' /opt/homebrew/bin $HOME/.maven/bin $HOME/.cargo/bin $HOME/go/bin $HOME/.local/bin $HOME/bin /usr/local/sbin /usr/local/bin
+fish_add_path '/Applications/Visual Studio Code.app/Contents/Resources/app/bin' '/Applications/IntelliJ IDEA.app/Contents/MacOS' /opt/homebrew/bin /opt/homebrew/sbin $HOME/.maven/bin $HOME/.cargo/bin $HOME/go/bin $HOME/.local/bin $HOME/bin /usr/local/sbin /usr/local/bin
 
 if type -q nvim
   abbr -a vim nvim
