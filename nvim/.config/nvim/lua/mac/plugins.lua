11c29b60958899fade931b777b882c2ba8e8cb3c
Matt Cordial <matt.cordial@experian.com>
2024-12-12 18:47:04 -0500

    add completion plugins

--- nvim/.config/nvim/lua/mac/plugins.lua
+++ nvim/.config/nvim/lua/mac/plugins.lua
@@ -11,7 +11,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
 local plugins = {
     'editorconfig/editorconfig-vim',
     'RRethy/nvim-base16',
@@ -138,11 +137,47 @@ local plugins = {
     },
     { -- optional cmp completion source for require statements and module annotations
         "hrsh7th/nvim-cmp",
-        opts = function(_, opts)
-            opts.sources = opts.sources or {}
-            table.insert(opts.sources, {
-                name = "lazydev",
-                group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+        dependencies = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-nvim-lsp-document-symbol',
+            'hrsh7th/cmp-nvim-lsp-signature-help',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            'hrsh7th/cmp-vsnip',
+            'hrsh7th/vim-vsnip',
+        },
+        config = function(opts)
+            local cmp = require('cmp')
+            cmp.setup({
+                snippet = {
+                  expand = function(args)
+                    vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+                  end,
+                },
+                window = {
+                  completion = cmp.config.window.bordered(),
+                  documentation = cmp.config.window.bordered(),
+                },
+                mapping = cmp.mapping.preset.insert({
+                  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                  ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                  ['<C-Space>'] = cmp.mapping.complete(),
+                  ['<C-e>'] = cmp.mapping.abort(),
+                  ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                }),
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'nvim_lsp_document_symbol' },
+                    { name = 'nvim_lsp_signature_help' },
+                    { name = 'vsnip' },
+                    {
+                        name = "lazydev",
+                        group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+                    }
+                }, {
+                  { name = 'buffer' },
+                })
             })
         end,
     },
