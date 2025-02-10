a27efa8a4daadb8b6ef8f23c2304a9378942d64b
Matt Cordial <matt.cordial@gmail.com>
2025-02-09 22:24:47 -0700

    clean up keymaps

--- nvim/.config/nvim/plugin/keymaps.lua
+++ nvim/.config/nvim/plugin/keymaps.lua
@@ -1,3 +1,4 @@
+local fzf = require('fzf-lua')
 local map = vim.keymap.set
 
 local opt_defaults = { silent = true, noremap = true }
@@ -12,32 +13,27 @@ nmap('<c-k>', ':bp <cr>')
 
 nmap('<leader>w', ':bp<cr>:bd #<cr>')
 nmap('<leader>-', ':Oil<cr>')
-
-nmap('<leader>cr', vim.lsp.buf.rename)
-nmap('<leader>ca', vim.lsp.buf.code_action)
-nmap('<leader>ci', vim.lsp.buf.implementation)
-
-nmap('<leader>gs', ':Git<cr>')
-nmap('<leader>gp', ':Git pull<cr>')
-nmap('<leader>gf', ':Git fetch --all --prune --jobs=10<cr>')
-
 nmap("<leader>//", function()
     vim.api.nvim_feedkeys(':%s/', 'n', false)
 end)
 
--- Telescope
-local fzf = require('fzf-lua')
+nmap('<leader>gs', ':Git<cr>')
+nmap('<leader>gp', ':Git pull<cr>')
+nmap('<leader>gf', ':Git fetch --all --prune --jobs=10<cr>')
+nmap('<leader>gc', fzf.git_commits)
+nmap('<leader>gb', fzf.git_branches)
+
 nmap('<leader>fr', fzf.resume)
 nmap('<leader>ff', fzf.files)
 nmap('<leader>fs', ':FzfLua live_grep winopts.preview.hidden=false<cr>')
 nmap('<leader>fb', fzf.buffers)
 nmap('<leader>fu', fzf.lsp_references)
-nmap('<leader>gc', fzf.git_commits)
-nmap('<leader>gb', fzf.git_branches)
-nmap('<leader>ca', fzf.lsp_code_actions)
-nmap('<leader>cv', ':FzfLua files cwd=$HOME/.config/nvim fd_opts=--follow<cr>')
 
+nmap('<leader>ca', fzf.lsp_code_actions)
 nmap('<leader>ch', fzf.command_history)
+nmap('<leader>cr', vim.lsp.buf.rename)
+nmap('<leader>cv', ':FzfLua files cwd=$HOME/.config/nvim fd_opts=--follow<cr>')
+nmap('<leader>cf', ':FzfLua files cwd=$HOME/.config/fish fd_opts=--follow<cr>')
 
 nmap('<leader>p', fzf.zoxide)
 
