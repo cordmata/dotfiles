60fa6dc5adc95de60070caa9c5ff53af2c225e27
Matt Cordial <matt.cordial@gmail.com>
2025-02-16 14:12:05 -0700

    enable Fish vim mode

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -68,5 +68,11 @@ starship init fish | source
 zoxide init fish | source
 
 function fish_user_key_bindings
+    fish_default_key_bindings -M insert
+    # Then execute the vi-bindings so they take precedence when there's a conflict.
+    # Without --no-erase fish_vi_key_bindings will default to
+    # resetting all bindings.
+    # The argument specifies the initial mode (insert, "default" or visual).
+    fish_vi_key_bindings --no-erase insert
     fzf_key_bindings
 end
