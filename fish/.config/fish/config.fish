0007c6afffd3c5ad7e3958fc048d8f470d240672
Matt Cordial <matt.cordial@gmail.com>
2025-02-21 16:48:44 -0700

    no vim on cli

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -68,11 +68,6 @@ starship init fish | source
 zoxide init fish | source
 
 function fish_user_key_bindings
-    fish_default_key_bindings -M insert
-    # Then execute the vi-bindings so they take precedence when there's a conflict.
-    # Without --no-erase fish_vi_key_bindings will default to
-    # resetting all bindings.
-    # The argument specifies the initial mode (insert, "default" or visual).
-    fish_vi_key_bindings --no-erase insert
+    fish_default_key_bindings
     fzf_key_bindings
 end
