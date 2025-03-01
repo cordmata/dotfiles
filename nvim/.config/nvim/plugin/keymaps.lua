076e589b0ae9d622bb8fd752420a972a93270e67
Matt Cordial <matt.cordial@gmail.com>
2025-03-01 13:34:47 -0700

    update keymap for json formatting + add comments

--- nvim/.config/nvim/plugin/keymaps.lua
+++ nvim/.config/nvim/plugin/keymaps.lua
@@ -8,27 +8,34 @@ local function nmap(keymap, command, opts)
     map('n', keymap, command, opts)
 end
 
+-- buffer navigation
 nmap('<c-j>', ':bn <cr>')
 nmap('<c-k>', ':bp <cr>')
 nmap('<c-c>', ':bd <cr>')
 
+-- filesystem browse
 nmap('<leader>-', ':Oil<cr>')
+
+-- quick search/replace
 nmap("<leader>//", function()
     vim.api.nvim_feedkeys(':%s/', 'n', false)
 end)
 
+-- git
 nmap('<leader>gs', ':Git<cr>')
 nmap('<leader>gp', ':Git pull<cr>')
 nmap('<leader>gf', ':Git fetch --all --prune --jobs=10<cr>')
 nmap('<leader>gc', fzf.git_commits)
 nmap('<leader>gb', fzf.git_branches)
 
+-- common fuzzy-finders
 nmap('<leader>fr', fzf.resume)
 nmap('<leader>ff', fzf.files)
 nmap('<leader>fs', ':FzfLua live_grep winopts.preview.hidden=false<cr>')
 nmap('<leader>fb', fzf.buffers)
 nmap('<leader>fu', fzf.lsp_references)
 
+-- manage scratch buffer
 nmap('<leader>bs', function ()
     local wins = vim.api.nvim_tabpage_list_wins(0)
     local scratch = vim.fn.bufnr('__Scratch__', true)
@@ -46,18 +53,22 @@ nmap('<leader>bs', function ()
     end
 end)
 
+-- code and configuration
 nmap('<leader>ca', fzf.lsp_code_actions)
 nmap('<leader>ch', fzf.command_history)
 nmap('<leader>cr', vim.lsp.buf.rename)
 nmap('<leader>cv', ':FzfLua files cwd=$HOME/.config/nvim<cr>')
 nmap('<leader>cf', ':FzfLua files cwd=$HOME/.config/fish<cr>')
 
+-- diagnostics
 nmap('<leader>dd', vim.diagnostic.open_float)
 nmap('<leader>ds', vim.diagnostic.show)
 nmap('<leader>dh', vim.diagnostic.hide)
 
-nmap('<leader>jq', ':%!jq .<cr>')
+-- json format
+nmap('<leader>jf', ':%!jq .<cr>')
 
+-- project switcher
 nmap('<leader>p', function()
     fzf.zoxide({
         actions = {
@@ -69,7 +80,11 @@ nmap('<leader>p', function()
     })
 end)
 
+-- enable/disable inline spell checking
 nmap('<leader>ss', ':set invspell<cr>')
 
+-- list all fuzzy-find tools available
 nmap('<leader>zz', ':FzfLua<cr>')
-map('t', '<esc><esc>', "<c-\\><c-n>") -- use esc to return to normal mode when in terminal mode
+
+-- use esc to return to normal mode when in terminal mode
+map('t', '<esc><esc>', "<c-\\><c-n>")
