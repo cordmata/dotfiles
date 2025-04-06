27d5925c38622c1e1dfabbd8c526c0ecd8e8085a
Matt Cordial <matt.cordial@gmail.com>
2025-04-06 09:46:45 -0700

    update borders file for nvim 0.11

--- nvim/.config/nvim/plugin/borders.lua
+++ nvim/.config/nvim/plugin/borders.lua
@@ -14,8 +14,8 @@ local border = {
 
 -- LSP settings (for overriding per client)
 local handlers =  {
-  ["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = border}),
-  ["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = border }),
+  ["textDocument/hover"] =  vim.lsp.buf.hover({border = border}),
+  ["textDocument/signatureHelp"] =  vim.lsp.buf.signature_help({border = border }),
 }
 
 -- To instead override globally
