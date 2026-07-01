681e2ca1fbffc67ad80fbfbc2e8558d4cb54fea7
Matt Cordial <Matthew.Cordial@geaerospace.com>
2026-07-01 17:12:38 -0400

    allow searching hidden files, but skip modules, git, etc.

--- nvim/.config/nvim/lua/mac/plugins/fzf.lua
+++ nvim/.config/nvim/lua/mac/plugins/fzf.lua
@@ -10,6 +10,21 @@ return  {
                     preview = { hidden = true },
                 },
                 files = { follow = true },
+                grep = {
+                    rg_opts = "\z
+                        --hidden \z
+                        --glob=!.git/* \z
+                        --glob=!node_modules/* \z
+                        --glob=!pnpm-lock.yaml \z
+                        --glob=!package-lock.json \z
+                        --column \z
+                        --line-number \z
+                        --no-heading \z
+                        --color=always \z
+                        --smart-case \z
+                        --max-columns=4096 \z
+                        -e"
+                },
             }
             fzf.register_ui_select()
         end
